9363631ee781ea5d0c7f6831ee23398094be587e
Elisama15Nkula <elisamitita@gmail.com>
2024-06-05 13:32:22 +0100

    Adicionado novo codigo para a tela login

---
+++ soga-rede-social-Elisama-Branche/src/paginas/login/Login.jsx
@@ -0,0 +1,7 @@
+import style from "./Login.module.css";
+
+function Login() {
+  return <div className={style.login}>Login - ready</div>;
+}
+
+export default Login;
---
+++ soga-rede-social-Elisama-Branche/src/paginas/registrar/Registrar.jsx
@@ -0,0 +1,9 @@
+import style from "./Resgistrar.module.css";
+
+function Registrar() {
+  return (
+    <div className={style.divContainer}>Registrar Todos os codigos html</div>
+  );
+}
+
+export default Registrar;
